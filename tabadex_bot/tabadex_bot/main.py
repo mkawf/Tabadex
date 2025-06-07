9eb124b57f5b048d3c4b796677be9e2f5334e654
Mohammad Kordalivand <120604640+mkawf@users.noreply.github.com>
2025-06-07 11:15:42 +0330

    Add files via upload

--- tabadex_bot/tabadex_bot/main.py
+++ tabadex_bot/tabadex_bot/main.py
@@ -10,12 +10,12 @@ from .database.models import Base
 from .utils.swapzone_api import swapzone_api_client
 
 # --- Import All Handlers with correct names ---
-from .handlers.start_handler import start_handler, language_handler  # <<<--- اصلاح شده
+from .handlers.start_handler import start_handler, language_handler
 from .handlers.menu_handler import menu_handler
 from .handlers.exchange_handler import exchange_handler
 from .handlers.account_handler import add_address_conv_handler, account_handlers
 from .handlers.support_handler import create_ticket_conv, reply_ticket_conv, support_handlers
-from .handlers.admin.panel_handler import admin_panel_handler
+from .handlers.admin.panel_handler import admin_panel_callback_handler, admin_panel_entry_handler
 from .handlers.admin.ticket_management import admin_reply_conv, admin_ticket_handlers
 from .handlers.admin.user_management import search_user_conv, admin_user_handlers
 from .handlers.admin.broadcast import broadcast_conv_handler
@@ -25,10 +25,13 @@ class DBSessionContext(ContextTypes.DEFAULT_TYPE):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._db_session: AsyncSession | None = None
+
     @property
     def db_session(self) -> AsyncSession:
-        if self._db_session is None: raise AttributeError("db_session is not set.")
+        if self._db_session is None:
+            raise AttributeError("db_session is not set.")
         return self._db_session
+
     @db_session.setter
     def db_session(self, value: AsyncSession):
         self._db_session = value
@@ -65,11 +68,12 @@ def main() -> None:
 
     # Command & Basic Handlers
     application.add_handler(start_handler)
-    application.add_handler(language_handler) # <<<--- اصلاح شده
+    application.add_handler(language_handler)
 
     # All other CallbackQueryHandlers and MessageHandlers from submenus
     all_other_handlers = [
-        *account_handlers, *support_handlers, admin_panel_handler,
+        *account_handlers, *support_handlers,
+        admin_panel_callback_handler, admin_panel_entry_handler,
         *admin_ticket_handlers, *admin_user_handlers, *admin_settings_handlers
     ]
     application.add_handlers(all_other_handlers)
